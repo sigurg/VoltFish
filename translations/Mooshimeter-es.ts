7ad9f51eaa9caca7f05551a7645fef50676e7cf6
GNU user <carlosgonz@protonmail.com>
2018-09-19 15:04:12 -0400

    Create Mooshimeter-es.ts

---
+++ translations/Mooshimeter-es.ts
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Inofficial Sailfish OS app for the &lt;a href=&quot;https://moosh.im/mooshimeter/&quot;&gt;Mooshimeter&lt;/a&gt; Bluetooth low energy multimeter.</source>
+        <translation>Un cliente no oficial para Sailfish OS de &lt;a href=&quot;https://moosh.im/mooshimeter/&quot;&gt;Mooshimeter.&lt;/a&gt; Multímetro de bajo consumo de energía Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Sampling Rate</source>
+        <translation>Frecuencia de muestreo</translation>
+    </message>
+    <message>
+        <source>Buffer Depth</source>
+        <translation>Profundidad de búfer</translation>
+    </message>
+    <message>
+        <source>Bat: </source>
+        <translation>Bat: </translation>
+    </message>
+    <message>
+        <source>Current D/C</source>
+        <translation>C/D actual</translation>
+    </message>
+    <message>
+        <source>Current A/C</source>
+        <translation>C/A actual</translation>
+    </message>
+    <message>
+        <source>Aux. Voltage D/C</source>
+        <translation>Aux. Voltaje C/D</translation>
+    </message>
+    <message>
+        <source>Aux. Voltage A/C</source>
+        <translation>Aux. Voltaje C/A</translation>
+    </message>
+    <message>
+        <source>Resistance</source>
+        <translation>Resistencia</translation>
+    </message>
+    <message>
+        <source>Diode</source>
+        <translation>Diodo</translation>
+    </message>
+    <message>
+        <source>Temperature</source>
+        <translation>Temperatura</translation>
+    </message>
+    <message>
+        <source>Voltage D/C</source>
+        <translation>Voltaje C/D</translation>
+    </message>
+    <message>
+        <source>Voltage A/C</source>
+        <translation>Voltaje C/A</translation>
+    </message>
+</context>
+<context>
+    <name>QMooshimeter</name>
+    <message>
+        <source>Real Power</source>
+        <translation>Potencia real</translation>
+    </message>
+    <message>
+        <source>Apparent Power</source>
+        <translation>Potencia aparente</translation>
+    </message>
+    <message>
+        <source>Power Factor</source>
+        <translation>Potencia factor</translation>
+    </message>
+    <message>
+        <source>out of range</source>
+        <translation>Fuera de rango</translation>
+    </message>
+    <message>
+        <source>invalid inputs</source>
+        <translation>Entradas inválidas</translation>
+    </message>
+    <message>
+        <source>Thermocouple (K)</source>
+        <translation>Termopar (K)</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Bluetooth Address</source>
+        <translation>Dirección Bluetooth</translation>
+    </message>
+    <message>
+        <source>Temperature Unit</source>
+        <translation>Unidad de temperatura</translation>
+    </message>
+    <message>
+        <source>Kelvin</source>
+        <translation>Kelvin</translation>
+    </message>
+    <message>
+        <source>Celcius</source>
+        <translation>Celcius</translation>
+    </message>
+    <message>
+        <source>Fahrenheit</source>
+        <translation>Fahrenheit</translation>
+    </message>
+    <message>
+        <source>Shipping Mode</source>
+        <translation>Modo de envío</translation>
+    </message>
+    <message>
+        <source>Reboot Mooshimeter</source>
+        <translation>Reiniciar Mooshimeter</translation>
+    </message>
+    <message>
+        <source>Enter shipping mode</source>
+        <translation>Entrando modo envío</translation>
+    </message>
+    <message>
+        <source>Rebooting Mooshimeter</source>
+        <translation>Reiniciando Mooshimeter</translation>
+    </message>
+</context>
+<context>
+    <name>util</name>
+    <message>
+        <source>Voltage A/C</source>
+        <translation>Voltaje C/A</translation>
+    </message>
+    <message>
+        <source>Voltage D/C</source>
+        <translation>Voltaje C/D</translation>
+    </message>
+    <message>
+        <source>Current A/C</source>
+        <translation>C/A actual</translation>
+    </message>
+    <message>
+        <source>Current D/C</source>
+        <translation>C/D actual</translation>
+    </message>
+    <message>
+        <source>Temperature</source>
+        <translation>Temperatura</translation>
+    </message>
+    <message>
+        <source>Aux. Voltage A/C</source>
+        <translation>Aux. Voltaje C/A</translation>
+    </message>
+    <message>
+        <source>Aux. Voltage D/C</source>
+        <translation>Aux. Voltaje C/D</translation>
+    </message>
+    <message>
+        <source>Resistance</source>
+        <translation>Resistencia</translation>
+    </message>
+    <message>
+        <source>Diode</source>
+        <translation>Diodo</translation>
+    </message>
+</context>
+</TS>
