b0b8b8a2e46a38a23e7defc21226035e5b7d0c8f
sigurg <sigurg@users.noreply.github.com>
2019-03-30 03:00:16 +0100

    added 172x172 icon, fixed desktop file

--- icons/gen_icons.sh
+++ icons/gen_icons.sh
@@ -3,7 +3,7 @@
 APP_NAME=voltfish
 cd "$(dirname "$(readlink -f "$0")")"
 
-for i in 86x86 108x108 128x128
+for i in 86x86 108x108 128x128 172x172
 do
 	convert -resize "$i" "256x256/${APP_NAME}.png" "${i}/${APP_NAME}.png"
 done
